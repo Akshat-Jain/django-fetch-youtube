7b9159bfef5948e40b76374285ab2d93d4cc7ee9
Akshat Jain <akjn11@gmail.com>
2020-04-17 03:06:05 +0530

    Fetching videos from youtube successfully v1.0

--- youtube/views.py
+++ youtube/views.py
@@ -3,19 +3,40 @@ from django.template import loader
 from django.http import HttpResponse
 from .models import YoutubeMetadata
 import json
+import requests
 
 # Create your views here.
 def index(request):
-    data = YoutubeMetadata.objects.all()
+
+    data = fetchVideos()
+
+    # data = YoutubeMetadata.objects.all()
     response = []
     
-    for i in data:
-    	entry = {}
-    	entry['title'] = i.title
-    	entry['description'] = i.description
-    	entry['publishTimestamp'] = str(i.publishTimestamp)
-    	entry['thumbnailUrl'] = i.thumbnailUrl
-    	response.append(entry)
+    for i in data['items']:
+        entry = {}
+        # entry['title'] = i.title
+        entry['title'] = i['snippet']['title']
+        entry['description'] = i['snippet']['description']
+        entry['publishedTimestamp'] = i['snippet']['publishedAt']
+        entry['thumbnailUrl'] = i['snippet']['thumbnails']['default']['url']
+        response.append(entry)
     
     res = json.dumps(response)
     return HttpResponse(res)
+
+def fetchVideos():
+    queryArguments = {
+    'order':'date',
+    'part':'snippet',
+    'publishedAfter':'2020-04-04T15:51:12.000Z',
+    'q':'mkbhd',
+    'type':'video',
+    'key':'AIzaSyAitzsrJ99oB7zwAHqdPb60RhAqwSVDGro'
+    }
+    url = 'https://content.googleapis.com/youtube/v3/search'
+    r = requests.get(url,params=queryArguments)
+    data = r.json()
+    return data
+# def backgroundTask(response):
+#   return json.dumps(response)
